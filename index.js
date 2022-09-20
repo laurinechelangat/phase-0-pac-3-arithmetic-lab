df23ba1d84584935e072f8499b2e9d7c71a6f792
laurine chelangat <laurinechelangat@gmail.com>
2022-09-20 15:42:16 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,60 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a,b){
+    return a+b
+
+}function substract (a, b){
+    return a-b
+}
+function multiply(a,b){
+    a*b
+}
+function divide(a,b){
+    return a/b
+}
+function increment(n){
+    return ++n;
+}
+function decrement(n){
+    return --n;
+}
+function makeInt(string){
+    return parseint(string,10)
+}
+function preserveDecimal(string){
+    return perseFloat(string)
+}
+function substract (a,b){
+    return a-b
+}
+ function subtract(a,b){
+    return a-b
+ }
+ function multiply(a,b){
+    return a*b
+ }
+ function makeInt(string){
+    return parseInt(string,10)
+ }
+ function preserveDecimal(string){
+    return perseFloat(string)
+ }
+ function preserveDecimal(string){
+    return parseFloat(string)
+ }
+ function preserveDecimal(n){
+    return parseFloat(string)
+ }
+ function preserveDecimal(string){
+    return parseFloat (string)
+ }
